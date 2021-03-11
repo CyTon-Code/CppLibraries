e20e38ba150df69624d73643598e5c58bc39b237
CyTon Code <cytoncode@gmail.com>
2021-03-11 17:05:33 +0200

    add code

---
+++ scripts/hello_world_1.cpp
@@ -0,0 +1,13 @@
+//
+// Created by cyton code on 11.03.2021.
+//
+
+#include <iostream>
+//#include"\lib\tmf.h"
+//#include "lib\tmf.h"
+///run/media/cytoncode/GIT/CppLibraries/lib/tmf.h
+int main() {
+//    std::string word = input("what's you name? ");
+//    hello_word(word);
+//    exit;
+}
