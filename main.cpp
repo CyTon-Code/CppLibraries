7e72fd0f27a1f91fa2b30a30b1f3fb8560c71548
CyTon Code <cytoncode@gmail.com>
2021-03-11 10:46:47 +0200

    upd code

--- main.cpp
+++ main.cpp
@@ -1,16 +1,36 @@
-// section inc
 #include <iostream>
-//#include "hello/hello.h"
+
 #include "lib/tmf.h"
 
-// section main
-int main(
-        // section len args
-        int argc,
-        // section array args
-        const char **args
-) {
+using std::cout, std::endl;
+//using namespace std;
 
-    for(i:args)cout << i<< ' ';
+// section main
+int main() {
+    int x1 = 10, x2 = 20;
+    cout << "x1 = " << x1 << ", x2 = " << x2 << endl;
+    swap(x1, x2); // Передаём ссылки
+    cout << "x1 = " << x1 << ", x2 = " << x2 << endl;
+
+    return 0;
+}
+
+
+
+/*
+int main() {
+    double x, a, c, b, y;
+
+    c = input("c = ");
+    a = input("a = ");
+    x = input("x = ");
+
+    b = pow(c, 3);
+    a = (a * b) + c;
+    x = pow(x * a, 2);
+    y = sin(a / x) + log10(0.08) * exp(x);
+
+    cout << "y= " << y << endl;
     exit;
 }
+ */
